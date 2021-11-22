b887be9e6bc15ceb055332d3b6188bfe1daade46
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2021-11-22 09:23:02 -0300

    add migrations

--- database/migrations/2021_11_22_120918_create_disciplines_table.php
+++ database/migrations/2021_11_22_120918_create_disciplines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostJobsTable extends Migration
+class CreateDisciplinesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreatePostJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_jobs', function (Blueprint $table) {
+        Schema::create('disciplines', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('document');
-            $table->boolean('visible')->default($value = true);
-            $table->foreignId('user_id');
-            $table->foreignId('period_id');
-            $table->foreignId('discipline_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreatePostJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_jobs');
+        Schema::dropIfExists('disciplines');
     }
 }
